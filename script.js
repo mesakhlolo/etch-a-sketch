c5878843799db1a3cf123ab133932ee3ed271927
Mesakh Lolo Meha <mesakhlolo12@gmail.com>
2023-05-22 13:54:58 +0700

    menjalankan button color dan rgb mode

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
+const colorBtn = document.getElementById("colorBtn");
+const rgbBtn = document.getElementById("rgbBtn");
+const clearBtn = document.getElementById("clearBtn");
+let color = "black";
+
 document.addEventListener("DOMContentLoaded", function () {
+  colorBtn.addEventListener("click", () => setColor("color"));
+  rgbBtn.addEventListener("click", () => setColor("rgb"));
+  clearBtn.addEventListener("click", clearBoard);
+
   createBoard(16);
 
   // to change board value using slider
-  let output = document.getElementById("rangeOutput");
-  let input = document.getElementById("rangeInput");
+  const output = document.getElementById("rangeOutput");
+  const input = document.getElementById("rangeInput");
   input.addEventListener("input", (event) => {
-    let size = input.value; // to get the input value (value dari slider)
+    const size = input.value; // to get the input value (value dari slider)
     createBoard(size);
     output.innerHTML = `Board: ${event.target.value} x ${event.target.value}`;
   });
@@ -13,18 +22,48 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function createBoard(size) {
   const board = document.getElementById("board");
-  board.innerHTML = ""; // Menghapus semua elemen sebelumnya
+  const fragment = document.createDocumentFragment(); // create DOM fragment
 
+  board.innerHTML = ""; // remove all previous element
   board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-  const fragment = document.createDocumentFragment(); // Membuat fragmen DOM
-
-  let numDivs = size * size;
+  const numDivs = size * size;
   for (let i = 0; i < numDivs; i++) {
-    let div = document.createElement("div");
-    fragment.appendChild(div); // Menambahkan elemen div ke dalam fragmen
+    const div = document.createElement("div");
+    div.addEventListener("mouseover", colorDiv);
+    fragment.appendChild(div); // add div element to fragment
   }
 
-  board.appendChild(fragment); // Menambahkan fragmen ke dalam elemen board
+  board.appendChild(fragment); // add fragment to board
+}
+
+function colorDiv() {
+  if (color === "rgb") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = "black";
+  }
+}
+
+function setColor(colorChoice) {
+  color = colorChoice;
+
+  // Remove the selected class from both buttons
+  colorBtn.classList.remove("button-selected");
+  rgbBtn.classList.remove("button-selected");
+
+  // Add the selected class to the active button
+  if (colorChoice === "color") {
+    colorBtn.classList.add("button-selected");
+  } else if (colorChoice === "rgb") {
+    rgbBtn.classList.add("button-selected");
+  }
+}
+
+function clearBoard() {
+  const divs = document.querySelectorAll("#board div");
+  divs.forEach((div) => {
+    div.style.backgroundColor = "";
+  });
 }
